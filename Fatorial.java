8c61c426c4935378667c504b061195a834be31cb
BrunoSousa4 <32168945+BrunoSousa4@users.noreply.github.com>
2017-12-14 16:12:47 +0000

    Update and rename JavaApplication5.java to Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -10,8 +10,7 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         int N, Fat=1;
         Scanner LerS= new Scanner (System.in);
-        System.out.print("Digite um numero positivo inferior a 32 : ");
-N=LerS.nextInt();
+FatorialN=LerS.nextInt();
 for (int M=N; M>=1; M--)
     Fat*=M;
 System.out.printf("%d!=%d#", N, Fat);
